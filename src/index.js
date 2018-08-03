39c8a240db6fe0ce7d6b1d493e63ccae78bc9929
Esteban Samuel Reyes <estebansamuel.reyes@gmail.com>
2018-08-03 18:23:21 -0500

    pages

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('cv'));
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('cv'));
 registerServiceWorker();
 
 
